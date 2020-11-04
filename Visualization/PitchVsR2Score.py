9987398e304238ed67c588a4166ec0cc1801f805
FullMetalNicky <nickyfullmetal@gmail.com>
2020-11-04 12:12:21 +0100

    adding 2 model plotting

--- Visualization/PitchVsR2Score.py
+++ Visualization/PitchVsR2Score.py
@@ -19,18 +19,14 @@ import sklearn.metrics
 import pandas as pd
 
 
-sys.path.append("../PyTorch/")
 
 
 
+def OffsetToPitch(h):
 
-vertical_range = 70
-half_vertical_range = vertical_range/2
-val_range = [-14.5, 14.5]
-
-
-def OffsetToPitch(p_test, h, vfov):
-    pitch = -(half_vertical_range - p_test) * vfov / h
+    vfov = 65.65
+    p_list = np.linspace(0, 160-h, 160-h)
+    pitch = -(0.5 * (160 - h) - p_list) * vfov / h
 
     return pitch
 
@@ -80,11 +76,9 @@ def CalculateR2ForPitch(outputs, gt_labels, range_p):
     return tot_x_r2, tot_y_r2, tot_z_r2, tot_phi_r2
 
 
-def PlotModelR2Score(ax, y_values, x_values, x_labels, len, skip, color, title, model_label):
+def PlotModelR2Score(ax, y_values, x_values, color, title, model_label):
     ax.plot(x_values, y_values, color=color, label=model_label)
     ax.set_title(title, fontsize=18)
-    ax.set_xticks(np.arange(0, len, skip))
-    ax.set_xticklabels(x_labels, rotation=30, fontsize=8)
     ax.set_xlabel('Pitch', fontsize=18)
     ax.set_ylabel('R2', fontsize=18)
     ax.set_ylim([0, 1])
@@ -101,48 +95,44 @@ def VizPitchvsR2ScoreSubPlots(ax, outputs, gt_labels, p_test, color, model_label
     range_p = max_p - min_p + 1
     outputs = np.reshape(outputs, (-1, range_p, 4))
     gt_labels = np.reshape(gt_labels, (-1, range_p, 4))
-    tot_pitch = list(range(range_p))
-    skip = 5
-    pitch_labels = np.linspace(-14, 14, 15, endpoint=True)
-    len_labels = len(tot_pitch) + 5
+    tot_pitch = OffsetToPitch(96)
 
     tot_x_r2, tot_y_r2, tot_z_r2, tot_phi_r2 = CalculateR2ForPitch(outputs, gt_labels, range_p)
 
-    PlotModelR2Score(ax[0][0], tot_x_r2, tot_pitch, pitch_labels, len_labels, skip, color, "Output variable: x", model_label)
-    PlotModelR2Score(ax[0][1], tot_y_r2, tot_pitch, pitch_labels, len_labels, skip, color, "Output variable: y", model_label)
-    PlotModelR2Score(ax[1][0], tot_z_r2, tot_pitch, pitch_labels, len_labels, skip, color, "Output variable: z", model_label)
-    PlotModelR2Score(ax[1][1], tot_phi_r2, tot_pitch, pitch_labels, len_labels, skip, color, "Output variable: phi", model_label)
-
+    PlotModelR2Score(ax[0][0], tot_x_r2, tot_pitch, color, "Output variable: x", model_label)
+    PlotModelR2Score(ax[0][1], tot_y_r2, tot_pitch, color, "Output variable: y", model_label)
+    PlotModelR2Score(ax[1][0], tot_z_r2, tot_pitch, color, "Output variable: z", model_label)
+    PlotModelR2Score(ax[1][1], tot_phi_r2, tot_pitch, color, "Output variable: phi", model_label)
 
     return range_p
 
 
-# def Plot2Models(p_test, name, base_r2_score):
-#
-#
-#     fig, ax = plt.subplots(2, 2, figsize=(16, 12))
-#     fig.suptitle("R2 Score as a function of Pitch", fontsize=22)
-#
-#     name1 = "pickles/DronetOthes160x90AugCropResults.pickle"
-#     outputs, gt_labels = LoadPerformanceResults(name1)
-#     range_p = VizPitchvsR2ScoreSubPlots(ax, outputs, gt_labels, p_test, 'b', 'Pitch-augmented')
-#
-#     name2 = "pickles/DronetOthes160x90VizAugResults.pickle"
-#     outputs, gt_labels = LoadPerformanceResults(name2)
-#     range_p = VizPitchvsR2ScoreSubPlots(ax, outputs, gt_labels, p_test, 'g', 'Non-augmented')
-#
-#     PlotBasePoint(ax, base_r2_score, (range_p + 1) / 2)
-#
-#     plt.subplots_adjust(hspace=0.3)
-#
-#
-#     if name.find(".pickle"):
-#         name = name.replace(".pickle", '')
-#     plt.savefig(name + '_pitch.png')
-#     plt.show()
+def Plot2Models(p_test, archive_name1, archive_name2, base_r2_score1):
 
 
-def Plot1Model(p_test, archive_name, model_name, base_r2_score):
+    fig, ax = plt.subplots(2, 2, figsize=(16, 12))
+    fig.suptitle("R2 Score as a function of Pitch", fontsize=22)
+
+
+    outputs, gt_labels = LoadPerformanceResults(archive_name1)
+    range_p = VizPitchvsR2ScoreSubPlots(ax, outputs, gt_labels, p_test, 'b', 'Pitch-input')
+
+    outputs, gt_labels = LoadPerformanceResults(archive_name2)
+    range_p = VizPitchvsR2ScoreSubPlots(ax, outputs, gt_labels, p_test, 'g', 'Pitch-augmented')
+
+    PlotBasePoint(ax, base_r2_score1, 0)
+
+    plt.subplots_adjust(hspace=0.3)
+
+
+    # if name.find(".pickle"):
+    #     name = name.replace(".pickle", '')
+    name = "modelcomparison"
+    plt.savefig(name + '_pitch.png')
+    plt.show()
+
+
+def Plot1Model(p_test, archive_name, base_r2_score):
 
     fig, ax = plt.subplots(2, 2, figsize=(16, 12))
     fig.suptitle("R2 Score as a function of Pitch")
@@ -152,7 +142,7 @@ def Plot1Model(p_test, archive_name, model_name, base_r2_score):
     range_p = VizPitchvsR2ScoreSubPlots(ax, outputs, gt_labels, p_test, 'b', 'new')
 
 
-    PlotBasePoint(ax, base_r2_score, (range_p + 1) / 2)
+    PlotBasePoint(ax, base_r2_score, 0)
 
     if archive_name.find(".pickle"):
         archive_name = archive_name.replace(".pickle", '')
@@ -169,9 +159,11 @@ def main():
     p_test = DataProcessor.GetPitchFromTestData(datset_path)
     base_r2_score = [0.8628, 0.7919, 0.7068, 0.4467]
 
-    print((32-0))
 
-    Plot1Model(p_test, archive_name, model_name, base_r2_score)
+    #Plot1Model(p_test, archive_name, model_name, base_r2_score)
+
+    archive_name2 = "../Scripts/Archive/Others160x96HimaxTest16_4_2020Cropped64.pickle"
+    Plot2Models(p_test, archive_name, archive_name2, base_r2_score)
 
 
 if __name__ == '__main__':
